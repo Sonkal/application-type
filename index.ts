e24ac69749830d35ab823ca55e533b7e5f65c86b
Miroslav Novak <miroslav.novak@gmail.com>
2017-05-05 10:50:44 +0200

    adding data type

---
+++ index.ts
@@ -0,0 +1,10 @@
+export class Application{
+    firstName: string;
+    lastName: string;
+    address: string;
+    personalId: string;
+    email: string;
+    phone: string;
+    phoneMother: string;
+    phoneFather: string;
+}
